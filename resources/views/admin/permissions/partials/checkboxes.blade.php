909872a43bd089a89865980e7e886bafca911024
Faustino Vasquez <vasquez.limon@gmail.com>
2021-07-23 15:46:39 -0700

    Adding icheck-bootstrap

--- resources/views/admin/permissions/partials/checkboxes.blade.php
+++ resources/views/admin/permissions/partials/checkboxes.blade.php
@@ -1,11 +1,12 @@
 <ul class="list-unstyled">
     @foreach ($permissions as $id => $name)
         <li>
-            <label>
+            <div class="icheck-primary">
                 <input name="permissions[]" type="checkbox" value="{{ $id }}"
+                    id="permission{{ $id }}"
                     {{ $model->permissions->contains($id) || collect(old('permissions'))->contains($id) ? 'checked' : '' }}>
-                {{ $name }}
-            </label>
+                <label for="permission{{ $id }}"> {{ $name }}</label>
+            </div>
         </li>
     @endforeach
 </ul>
--- resources/views/admin/roles/partials/checkboxes.blade.php
+++ resources/views/admin/roles/partials/checkboxes.blade.php
@@ -1,12 +1,12 @@
 <ul class="list-unstyled">
     @foreach ($roles as $role)
-        <li>
-            <label>
-                <input name="roles[]" type="checkbox" value="{{ $role->id }}"
+        <li class="icheck-primary">
+            <div class="icheck-primary">
+                <input name="roles[]" type="checkbox" value="{{ $role->id }}" id="role{{ $role->id }}"
                     {{ $user->roles->contains($role->id) ? 'checked' : '' }}>
-                {{ $role->name }} <br>
+                <label for="role{{ $role->id }}">{{ $role->name }}</label><br>
                 <small class="text-muted">{{ $role->permissions->pluck('name')->implode(', ') }}</small>
-            </label>
+            </div>
         </li>
     @endforeach
 </ul>
